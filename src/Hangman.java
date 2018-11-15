b4f71aea7367e535ef2337b245ae974eb92206f1
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-11-15 09:54:45 -0500

    Alpha start

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,6 +2,9 @@ public class Hangman {
 
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Hangman!!");
+        System.out.println("The rules are simple:");
+        System.out.println("You input a secret word or phrase and then guess without getting any letters wrong.");
+        System.out.println("Have fun and good luck!!");
     }
 
 }
