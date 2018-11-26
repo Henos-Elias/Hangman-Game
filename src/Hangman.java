6c1cc2ab48f14b168adb67d58e2a4b1fb0cb2fd2
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-11-26 08:01:04 -0500

    Beta

--- src/Hangman.java
+++ src/Hangman.java
@@ -13,21 +13,93 @@ public class Hangman {
             {
                 int tries = 0;
 
-                boolean iterated = false;
-                String temp = "";
-                String holder = "";
+                String storedword;
+                char[] charstring;
+                int length;
+                char[] censor;
+                int attempts = 0;
 
-                Scanner keyboard = new Scanner(System.in);
-                System.out.println("Enter your secret word or phrase:");
-                String word = keyboard.nextLine();
+                StringBuilder pastguesses = new StringBuilder();
+
+                Scanner typedword = new Scanner(System.in);
+                System.out.println("Enter word to guess: ");
+                storedword = typedword.nextLine();
+                storedword = storedword.toUpperCase();
+                length = storedword.length();
+
+                charstring = storedword.toCharArray();
+
+
+                censor = storedword.toCharArray();
+                System.out.println("Your secret word is: ");
+
+                for (int index = 0; index < length; index++) {
+                    censor[index] = '-';
+                }
+
+
+                while (String.valueOf(censor).equals(storedword) == false) {
+
+
+                    char charguess;
+                    String tempword;
+                    String tempstring;
+                    boolean correct = false;
+                    int times = 0;
+                    boolean repeated = false;
+
+
+                    for (int a = 0; a < length; a++) {
+                        System.out.print(censor[a]);
+                    }
+                    System.out.println();
+
+
+                    Scanner guess = new Scanner(System.in);
+                    System.out.println("Type your guess: ");
+                    tempword = guess.next();
+                    charguess = tempword.charAt(0);
+
+                    pastguesses.append(charguess);
+                    tempstring = pastguesses.toString();
+
+
+                    if (tempstring.lastIndexOf(charguess, tempstring.length() - 2) != -1) {
+                        System.out.println("You already guessed this letter! Guess again. Your previous guesses were: ");
+                        pastguesses.deleteCharAt(tempstring.length() - 1);
+                        System.out.println(tempstring.substring(0, tempstring.length() - 1));
+                        repeated = true;
+                    }
+
+
+                    if (repeated == false) {
+                        for (int index = 0; index < length; index++) {
+
+                            if (charstring[index] == Character.toUpperCase(charguess)) {
+
+                                censor[index] = Character.toUpperCase(charguess);
+                                correct = true;
+                                times++;
+                            }
+                        }
+                        if (correct == true) {
+                            System.out.println("The letter " + charguess + " is in the secret word! There are " + times + " " + charguess + " 's in the word. Revealing the letter(s): ");
+                        } else if (correct == false) {
+                            System.out.println("Sorry, the letter is not in the word. Your secret word looks like this:  ");
+                        }
+                        System.out.println();
+                    }
+                    attempts++;
+                }
+                System.out.println("You guessed the entire word " + storedword.toUpperCase() + " correctly! It took you " + attempts + " attempts!");
+
+
+
             }
         }
-
-
-
-
-
     }
+
+
 
     private static void hangManImage(int tries) {
         String display = "";
